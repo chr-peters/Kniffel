dacd8a5f50a6420ab3df8622a97c25cf757012d8
cxan96 <c.p.xanten@gmail.com>
2017-12-19 10:49:08 +0100

    setup.php now creates config.php

---
+++ setup.php
@@ -0,0 +1,33 @@
+<?php
+$config_file = './webcontent/config.php';
+ 
+$db_host = getenv('DB_HOST');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_passwd = getenv('DB_PASSWD');
+$path_mysql = getenv('MYSQL_PATH');
+ 
+if(!$db_host){
+    $db_host = 'http://auravendill.de';
+}
+if(!$db_name){
+    $db_name = 'kniffel';
+}
+if(!$db_user){
+    $db_user = 'phpmyadmin';
+}
+if(!$db_passwd){
+    $db_passwd = 'TODO: ENTER PW';
+}
+if(!$path_mysql){
+    $path_mysql = 'C:\xampp\mysql\bin\mysql';
+}
+ 
+$file_contents = "\$db_host = '" . $db_host . "';\n"
+    . "\$db_user = '" . $db_user . "';\n"
+    . "\$db_passwd = '" . $db_passwd . "';\n"
+    . "\$db_name = '" . $db_name . "';\n";
+ 
+// write database config to file
+file_put_contents($config_file, $file_contents);
+echo error_get_last();
